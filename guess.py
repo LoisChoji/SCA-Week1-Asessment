aad483dc758a96815d27b3203fc445545da513e4
LoisChoji <43037884+LoisChoji@users.noreply.github.com>
2020-06-06 01:30:46 +0100

    guess.py allows users to guess a number

---
+++ guess.py
@@ -0,0 +1,10 @@
+import random
+word = random.randint(0,20)
+user = int(input("Pick a number from 0 to 20: "))
+if user == word:
+    print("This is the right number")
+elif user > word:
+    print("The number is too high")
+else:
+    print("The number is too low")
+
